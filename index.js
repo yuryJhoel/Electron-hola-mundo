2b997f2d08b75a878721718524d08163de7518fe
yury <zionlenuz1@gmail.com>
2019-01-03 11:04:30 -0500

    Electron | Hola mundo | First

---
+++ index.js
@@ -0,0 +1,25 @@
+// const electron = require('electron');
+const {app, BrowserWindow} = require('electron');
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({width: 1200, height: 600})
+    win.loadFile('index.html')
+    win.webContents.openDevTools()
+    win.on('closed', ()=>{
+        win=null
+    })
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', ()=>{
+    if(process.platform !== 'darwin'){
+        app.quit()
+    }
+})
+
+app.on('activate', ()=>{
+    if(win=== null){
+        createWindow()
+    }
+})
